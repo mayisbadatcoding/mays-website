cc55d7359f62c221ec71e132dfcc963f00ac95a4
ope674c <burtonchase936@gmail.com>
2026-05-11 05:05:39 -0400

    Main Push

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+if (menuBtn && navMenu) {
+  menuBtn.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+}
